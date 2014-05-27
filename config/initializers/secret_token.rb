386b9f5a7b83d8c1c5277179230b4786921cf4e9
Maciej Kocyła <maciej.kocyla@gmail.com>
2014-05-27 16:33:59 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ebookshop::Application.config.secret_key_base = '71b740a0e2874fcafbaab0170b612ac53de6ee99a80cb8ec6c697614e474719d08888345b40b56bb03bf8b1ff9542594e5eb11be5e6600b2bbd6863b20e55ab6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ebookshop::Application.config.session_store :cookie_store, key: '_ebookshop_session'
